82df10552c5875bb2b0d74e534515b9ad5adf3cb
Rebecca Clarke <rclarke@linz.govt.nz>
2025-02-14 13:07:44 +1300

    chore: add comments to rename.py

--- lidar_qc/cli/commands/rename.py
+++ lidar_qc/cli/commands/rename.py
@@ -51,6 +51,7 @@ def rename(
     Rename a collection of files of either DEM, DSM or CL2.
     """
 
+    # Make dry-run always verbose
     if not write:
         verbose = True
 
@@ -65,13 +66,15 @@ def rename(
     if not write:
         logger.info('This is a dry-run, no files are being modified. To execute rename, use --write')
 
-    # find raster and pc sub-directories
+    # find raster and point cloud sub-directories
     raw_data_dirs = find_data_subdirs(input_dir,[],[])
 
     for raw_data_dir, cls in raw_data_dirs:
+        # get all the files from the subdirectories which match glob_pattern
         files: list[Path] = list(raw_data_dir.glob(cls.glob_pattern))
 
-        if write is None and limit is not None: # apply limit for dry-run if specified
+        # apply limit for dry-run if specified
+        if write is None and limit is not None: 
             files = files[:limit]
 
         file_infos = []
@@ -96,14 +99,19 @@ def rename(
 
         if file_infos:
             for file_info in file_infos:
-
+                
+                # Get the official tile for this file
                 official_tile = file_info.get_correct_tile().feature_properties()
 
+                # Define if this file type is Raster or Point Cloud, to help determine later which prefix and suffix to use
                 product_type = file_info.product_type.value if file_info.file_type == 'Raster' else 'CL2' if file_info.file_type == 'PointCloud' else None
 
+                # Build new file path
                 current_file_path = os.path.join(raw_data_dir, file_info.file_name + file_info.file_extension)
                 new_file_path = os.path.join(raw_data_dir, product_type + '_' + official_tile["sheet_code_id"] + '_' + survey_start_year + '_' + str(official_tile["scale"]) + '_' + official_tile["tile"] + file_info.file_extension)           
                 
+                
+                # If raster, accomodate tfw files if they exist.
                 tfw = False
                 if file_info.file_type == 'Raster':
                     if os.path.exists(Path(current_file_path).with_suffix('.tfw')):
@@ -111,13 +119,13 @@ def rename(
                         current_tfw_file = Path(current_file_path).with_suffix('.tfw')
                         new_tfw_file = Path(new_file_path).with_suffix('.tfw')
                         
-                
+                # Output file name change ito logger if this is a dry-run
                 if not write:
                     logger.debug(f'{current_file_path} --> {new_file_path}')
                     if tfw:
                         logger.debug(f'{current_tfw_file} --> {new_tfw_file}')
                 
-
+                # Execute write
                 if write:
                     rename_file(current_file_path,new_file_path)
                     if tfw:
